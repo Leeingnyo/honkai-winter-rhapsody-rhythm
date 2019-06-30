ef5ef82c162532d4f521d2e8a534e9ffc424416d
Leeingnyo <dyd1928@naver.com>
2019-07-01 01:36:27 +0900

    add announcement

--- pages/main/main.js
+++ pages/main/main.js
@@ -6,7 +6,12 @@ class Main {
       el('p', '키설명', el('br'), 'S(기계) 스페이스바(생물) L(이능)'),
       this.toFullscreenButton = el('button.winter-button.fullscreen-button', '전체화면'),
       this.toSelect = el('button.winter-button.start-button', '시작하기'),
-      this.muteButton = el('button.winter-button.mute-button', '배경음 끄기')
+      this.muteButton = el('button.winter-button.mute-button', '배경음 끄기'),
+      el('p', '모바일에서는 렉이 심할 수 있습니다. PC 권장. 언젠가 최적화 하겠습니다.'),
+      el('p', '몇 가지는 노래 영상을 찾지 못 하였고, 몇 가지는 노트를 다 만들지 못 했습니다. ',
+          '캐릭터에 맞는 노래 영상이 유튜브에 올라와있다면 알려주세요. ',
+          '그리고 공략캐릭터가 다른 경우가 있을텐데 그것도 알려주시면 감사하겠습니다.'),
+      el('small', ' v1.1.0', ' (2019-07-01)', { style: { position: 'absolute', right: '6%', bottom: '5%' } }),
     );
 
     this.toFullscreenButton.onclick = () => {
