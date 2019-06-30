b879ec485614f6e389cf7a492bd14bc84ca58d4b
Leeingnyo <dyd1928@naver.com>
2019-07-01 01:15:09 +0900

    replace alert to element popup
    alert 가 뜨면 fullscreen 이 해제됨

--- pages/prepare/prepare.js
+++ pages/prepare/prepare.js
@@ -6,7 +6,14 @@ class Prepare {
         this.selectedCharacterName = el('h3'),
         this.selectedCharacterMessage = el('p')
       ),
-      this.startButton = el('button.start-button.honkai-button', '탐색 임무 진행')
+      this.startButton = el('button.start-button.honkai-button', '탐색 임무 진행'),
+      this.popup = el('.popup', { style: { display: 'none' } },
+        this.popup = el('.popup-title', '알림'),
+        el('.popup-contents',
+          this.popupMessage = el('.popup-message'),
+          this.popupButton = el('button.popup-button.honkai-button', '확인')
+        )
+      )
     );
 
     this.backButton.onclick = () => {
@@ -14,18 +21,24 @@ class Prepare {
       view.update('select');
     };
     this.startButton.onclick = () => {
-      bgm.stop();
       effect.play('start');
       if (!this.song.videoId) {
-        alert('노래 영상을 찾지 못 했어...');
+        this.popupMessage.textContent = '노래 영상을 찾지 못 했어...';
+        this.popup.style.display = 'block';
         return;
       }
       if (!(this.song.notes && this.song.notes.length)) {
-        alert('아직 노래가 구현되지 않았어!');
+        this.popupMessage.textContent = '아직 노래가 구현되지 않았어!';
+        this.popup.style.display = 'block';
         return;
       }
 
+      bgm.stop();
       view.update('loading', this.song);
+    };
+    this.popupButton.onclick = () => {
+      effect.play('clickButton');
+      this.popup.style.display = 'none';
     };
   }
 
