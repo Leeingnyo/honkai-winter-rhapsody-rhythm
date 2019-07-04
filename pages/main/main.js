63f0ba832fc241d42b4cca31e58f83790d95bf1c
Leeingnyo <dyd1928@naver.com>
2019-07-04 20:25:19 +0900

    fix message

--- pages/main/main.js
+++ pages/main/main.js
@@ -3,7 +3,7 @@ class Main {
     this.el = el('#main.screen',
       el('h2', '붕괴 3rd 윈터 랩소디 리듬 게임'),
       el('p', '모바일에서는 시작하기 전에 전체화면을 눌러 전체화면으로 만드는 것을 권장합니다. 효과음 소리가 작아서 배경음도 작게 설정했습니다. 디바이스 볼륨에 주의해주세요.'),
-      el('p', '키설명', el('br'), 'S(기계) 스페이스바(생물) L(이능)'),
+      el('p', { style: { color: 'red', fontWeight: 'bold', textAlign: 'center' } }, '키설명 - PC', el('br'), 'S(기계) 스페이스바(생물) L(이능)', el('br'), '모바일은 버튼 클릭'),
       this.toFullscreenButton = el('button.winter-button.fullscreen-button', '전체화면'),
       this.toSelect = el('button.winter-button.start-button', '시작하기'),
       this.muteButton = el('button.winter-button.mute-button', '배경음 끄기'),
@@ -13,9 +13,7 @@ class Main {
           '버퍼링일 때 게임이 멈추게 되어 플레이가 어려울 수 있으니 ',
           '가급적 통신 환경이 원할한 곳에서 해주시길 바립니다.'),
       el('p.message', '몇 가지는 노래 영상을 찾지 못 하였고, 몇 가지는 노트를 다 만들지 못 했습니다. ',
-          '캐릭터에 맞는 노래 영상이 유튜브에 올라와있다면 알려주세요. ',
-          '그리고 공략캐릭터의 이름이 틀리거나 ',
-          '실제 맵과 다른 경우가 있을텐데 그것도 알려주시면 감사하겠습니다.'),
+          '캐릭터에 맞는 노래 영상이 유튜브에 올라와있다면 알려주세요.'),
       el('small', ' v1.1.1', ' (2019-07-01)', { style: { position: 'absolute', right: '6%', bottom: '5%' } }),
     );
 
