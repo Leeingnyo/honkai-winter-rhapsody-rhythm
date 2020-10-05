f2484f054a59bd856f5e70c3cb8a77f90ab6ffc8
Leeingnyo <dyd1928@naver.com>
2020-10-05 10:50:52 +0900

    implement end page

--- pages/end/end.js
+++ pages/end/end.js
@@ -1,11 +1,28 @@
 class End {
   constructor() {
-    this.el = el('#end.screen');
+    this.el = el('#end.screen',
+      this.maxComboWrapper = el('.max-combo-wrapper',
+        el('.max-combo-message', el('div', '최대 콤보 수')),
+        el('.max-combo-text', this.maxComboText = text())
+      ),
+      this.click = el('.click-pad')
+    );
+
+    var next = () => {
+      window.removeEventListener('keydown', next);
+      effect.play('clickButton');
+      bgm.play();
+      view.update('select');
+    };
+
+    this.click.onclick = next;
+    window.addEventListener('keydown', next);
   }
 
   update() {
-    bgm.play();
-    view.update('select');
     sound.cleanup();
+    effect.play('clear');
+
+    this.maxComboText.textContent = game.combo.maxCombo;
   }
 }
