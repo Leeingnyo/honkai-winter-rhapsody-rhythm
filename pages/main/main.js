05c52615c590a4ad6c9e87f7d357ea43aff6861a
Leeingnyo <dyd1928@naver.com>
2020-10-05 10:55:51 +0900

    version up, fix message

--- pages/main/main.js
+++ pages/main/main.js
@@ -3,18 +3,17 @@ class Main {
     this.el = el('#main.screen',
       el('h2', '붕괴 3rd 윈터 랩소디 리듬 게임'),
       el('p', '모바일에서는 시작하기 전에 ', el('span', { style: { fontWeight: 'bold' } }, '전체화면을 눌러'),
-          ' 전체화면으로 만드는 것을 권장합니다. 또, 웬만하면 화면을 가로로 해주세요. 효과음 소리가 작아서 배경음도 작게 설정했습니다. ',
-          '디바이스 볼륨에 주의해주세요. 문제가 생기면 새로고침'),
+          ' 전체화면으로 만드는 것을 권장합니다. 웬만하면 화면을 가로로 해주세요. 효과음 소리가 작아서 배경음도 작게 설정했습니다. ',
+          '디바이스 볼륨에 주의해주세요. 문제가 생기면 그냥 새로고침 해주세요'),
       this.toFullscreenButton = el('button.winter-button.fullscreen-button', '전체화면'),
       this.toSelect = el('button.winter-button.start-button', '시작하기'),
       this.muteButton = el('button.winter-button.mute-button', '배경음 끄기'),
-      el('p.message', '모바일에서는 렉이 심할 수 있습니다. 언젠가 최적화 하겠습니다. PC 권장. Internet Explorer 사절.'),
       el('p.message', ' 플레이할 노래는 유튜브 영상으로 가져오는데, ',
           '버퍼링일 때 게임이 멈추게 되어 플레이가 어려울 수 있으니 ',
           '가급적 통신 환경이 원할한 곳에서 해주시길 바립니다.'),
-      el('p.message', '몇 가지는 노래 영상을 찾지 못 하였고, 몇 가지는 노트를 다 만들지 못 했습니다. ',
+      el('p.message', '몇 가지는 노래 영상을 찾지 못 했고, 몇 가지는 노트를 다 만들지 못 했습니다. ',
           '캐릭터에 맞는 노래 영상이 유튜브에 올라와있다면 알려주세요.'),
-      el('small', ' v1.1.6', ' (2020-10-04)', { style: { position: 'absolute', right: '6%', bottom: '5%' } }),
+      el('small', ' v1.1.7', ' (2020-10-05)', { style: { position: 'absolute', right: '6%', bottom: '5%' } }),
     );
 
     this.toFullscreenButton.onclick = () => {
